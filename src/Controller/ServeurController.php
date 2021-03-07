ff37b2efbfbda1d01d4e6262ef4fdc6ee7b859f1
aida <aida.mbengue@etu.univ-st-etienne.fr>
2021-03-08 00:44:17 +0100

    methode post et renvoi

--- src/Controller/ServeurController.php
+++ src/Controller/ServeurController.php
@@ -17,12 +17,28 @@ class ServeurController extends AbstractController
             'controller_name' => 'ServeurController',
         ]);
     }
+	 /**
+     * @Route("/login", name="create_login")
+     */
+    public function createlogin(Request $request,EntityManagerInterface
+	$manager): Response
+    { 
+	$login = new login();
+	$login=$request->request->get("name");
+	$login->setlogin($name);
+	$manager->persist($login);
+	$manager->flush();
+        return $this->render('login/index.html.twig', [
+            'name' => 'messsage',
+        ]);
+    }
+	
 
     /**
      * @Route("/formulaire", name="formulaire")
      */
     public function formulaire(): Response
-    {
+    { 
         return $this->render('serveur/formulaire.html.twig', [
             'controller_name' => 'formulaireController',
         ]);
@@ -37,4 +53,6 @@ class ServeurController extends AbstractController
             'controller_name' => 'utilisateurController',
         ]);
     }
+	
+	
 }
